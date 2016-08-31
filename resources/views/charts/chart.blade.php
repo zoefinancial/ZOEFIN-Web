fee506a34da72582c4c51a3bb1cbf87a19665916
migfru <migfru@gmail.com>
2016-08-31 12:13:28 -0500

    Visual changes -loading icon and animation

--- resources/views/charts/chart.blade.php
+++ resources/views/charts/chart.blade.php
@@ -2,7 +2,7 @@
     <div class="box-header with-border">
         <h3 class="box-title">{{ $box_title }}</h3>
         <div class="box-tools pull-right">
-            <i id="{{ $canvas_id }}_loading" class="fa-li fa fa-spinner fa-spin"></i>
+            <i id="{{ $canvas_id }}_loading" class="fa fa-spinner fa-pulse fa-fw"></i>
             <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
             </button>
             <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
