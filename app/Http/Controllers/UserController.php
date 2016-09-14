f1045df0d460a261717670d7d84abcf61f6daf78
Jose Alberto Gonzalez Rouille <spin.jag@gmail.com>
2016-09-14 11:07:48 -0500

    user controller

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+
+}
