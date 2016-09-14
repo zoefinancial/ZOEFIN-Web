041ca5ffcf0f90c34096f2d5ec288c79d1742d85
Jose Alberto Gonzalez Rouille <spin.jag@gmail.com>
2016-09-13 22:10:09 -0500

    remove fields

--- database/migrations/2016_09_09_031505_create_on_boarding_table.php
+++ database/migrations/2016_09_09_031505_create_on_boarding_table.php
@@ -16,8 +16,6 @@ class CreateOnBoardingTable extends Migration {
 		{
 			$table->increments('id');
 			$table->integer('marital_status_id')->index('fk_user_profiles_marital_status_idx');
-            $table->integer('relationship_types_id')->index('fk_on_boarding_relationship_types_idx');
-            $table->integer('relative_id')->nullable()->index('fk_on_boarding_on_boarding_idx');
 			$table->date('date_birth');
 			$table->string('gender', 45)->nullable();
 			$table->integer('income');
