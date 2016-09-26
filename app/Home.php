0257d2aef19fea6c1538b01c2cbb646e8147d6e5
alberto <spin.jag@gmail.com>
2016-09-26 15:27:07 -0500

    Validate home

---
+++ app/Home.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Home extends Model
+{
+    protected $fillable = ['users_id','home_types_id','investments_id', 'address', 'city', 'state', 'zip','current_value'];
+}
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -33,6 +33,8 @@ Route::get('/dashboard',
     ]
 );
 
+Route::post('/api/home','HomeController@store');
+
 Route::get('/insurance',
     ['middleware' => 'auth',
         function () {
@@ -313,4 +315,4 @@ Route::post('/deleteFile',
 
 Route::auth();
 
-Route::get('/home', ['middleware' => 'auth']);
+//Route::get('/home', ['middleware' => 'auth']);
