a894e6846ff593d0698b537d7211e66f1182ac00
alberto <spin.jag@gmail.com>
2016-09-28 13:39:04 -0500

    recover details of associated tables

--- app/BankingAccount.php
+++ app/BankingAccount.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class BankingAccount extends Model
 {
-    //
+    protected $fillable = ['users_id', 'banks_id', 'account_types_id', 'account_status_id', 'number', 'current_balance'];
+
+    public function getAccountType()
+    {
+        return $this->hasOne('App\AccountType','id','account_types_id');
+    }
+
+    public function getBank()
+    {
+        return $this->hasOne('App\Bank','id','banks_id');
+    }
+
+    public function getAccountStatus()
+    {
+        return $this->hasOne('App\AccountStatus','id','account_status_id');
+    }
 }
