cb1f7de57c5311d8c6d7c5d334266cd3b98d5ed1
alberto <spin.jag@gmail.com>
2016-10-03 15:00:49 -0500

    Populated seed for investment companies

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -20,5 +20,6 @@ class DatabaseSeeder extends Seeder
         $this->call(AccountStatusTableSeeder::class);
         $this->call(BanksTableSeeder::class);
         $this->call(LoanTypesTableSeeder::class);
+        $this->call(InvestmentCompaniesTableSeeder::class);
     }
 }
