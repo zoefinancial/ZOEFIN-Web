edb65ed1e03d15a52a6a71091e29e59ff73612bf
alberto <spin.jag@gmail.com>
2016-10-03 15:57:26 -0500

    Ordering code, add investment's widget

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -208,7 +208,7 @@
                                     </div>
                                     @push('scripts')
                                     <script>
-                                        $('#c_b_{{ $i }}').on('click', function (e) {createBankigAccount();});
+                                        $('#c_b_{{ $i }}').on('click', function (e) {createBankingAccount();});
                                     </script>
                                     @endpush
                                     <div class="box-body no-border">
@@ -241,6 +241,54 @@
                                     </div>
                                 </div>
                             </li>
+{{----}}
+                            <li>
+                                <div class="box box-primary collapsed-box box-solid bg-transparent">
+                                    <div class="box-header">
+                                        <h3 class="box-title">Investment account</h3>
+                                        <div class="box-tools pull-right">
+                                            <button type="button" class="btn btn-box-tool" id="c_in_{{ $i }}" title="Add Investment manually"><i class="fa fa-plus"></i>
+                                            </button>
+                                            <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-angle-down"></i>
+                                            </button>
+                                        </div>
+                                    </div>
+                                    @push('scripts')
+                                    <script>
+                                        $('#c_in_{{ $i }}').on('click', function (e) {createInvestment();});
+                                    </script>
+                                    @endpush
+                                    <div class="box-body no-border">
+                                        @foreach($userBankingAccounts as $bankingAccount)
+                                            @php
+                                                $i++;
+                                            @endphp
+                                            <div class="info-box bg-aqua row-hidden">
+                                                <span class="info-box-icon"><i class="fa fa-money"></i></span>
+                                                <span class="pull-right hover-btn">
+                                                <span id="e_in_{{ $i }}" class="label label-primary" title="Edit"><i class="fa fa-edit"></i></span>
+                                                <span id="d_in_{{ $i }}" class="label label-danger" title="Delete"><i class="fa fa-trash"></i></span>
+                                            </span>
+                                                <div class="info-box-content">
+                                                    <span class="info-box-text">Banking account</span>
+                                                    <span class="info-box-number" title="${{ titleMoney($bankingAccount->current_balance) }}">${{ formatMoney($bankingAccount->current_balance) }}</span>
+                                                    <div class="progress">
+                                                        <div class="progress-bar" style="width: 100%" ></div>
+                                                    </div>
+                                                    <span class="progress-description"></span>
+                                                </div>
+                                            </div>
+                                            @push('scripts')
+                                            <script>
+                                                $('#e_in_{{ $i }}').on('click', function (e) {editInvestment('{{ base64_encode($bankingAccount->id) }}','{{ $bankingAccount->banks_id }}','{{ $bankingAccount->account_types_id }}','{{ $bankingAccount->account_status_id }}','{{ $bankingAccount->number }}','{{ $bankingAccount->current_balance }}');});
+                                                $('#d_in_{{ $i }}').on('click', function (e) {deleteInvestment('{{ base64_encode($bankingAccount->id) }}');});
+                                            </script>
+                                            @endpush
+                                        @endforeach
+                                    </div>
+                                </div>
+                            </li>
+{{----}}
                         </ul>
                     </li>
                     <li class="unstyled-list"><a title="What i owe">Liabilities</a>
@@ -404,55 +452,6 @@
     ))
 
 
-{{--Create Banking account form--}}
-@include('layouts.forms.modal_form',
-    array(
-        'id'=>'modal_banking_account_form',
-        'header'=>'Create Bank Account',
-        'description'=>'',
-        'cancel_button_label'=>'Cancel',
-        'inputs'=>[
-            ['label'=>'Bank Name', 'name' => 'banks_id','id'=>'banks-id','type'=>'select', 'options' => $banksSelect],
-            ['label'=>'Account Type', 'name' => 'account_types_id','id'=>'account-types-id','type'=>'select', 'options' => $accountTypesSelect],
-            ['label'=>'Account Status', 'name' => 'account_status_id','id'=>'account-status-id','type'=>'select', 'options' => $accountStatusSelect],
-            ['label'=>'Account Number', 'name' => 'number','id'=>'number','type'=>'text'],
-            ['label'=>'Current Balance', 'name' => 'current_balance','id'=>'current-balance','type'=>'money'],
-        ],
-        'submit_button_label'=>'Create Bank Account','url'=>'/api/bankingaccount'
-    ))
-
-{{-- Edit banking account form --}}
-@include('layouts.forms.modal_form',
-    array(
-        'id'=>'modal_edit_banking_account_form',
-        'header'=>'Edit Bank Account',
-        'description'=>'',
-        'method'=>'put',
-        'cancel_button_label'=>'Cancel',
-        'inputs'=>[
-            ['label'=>'Bank Name', 'name' => 'banks_id','id'=>'banks-id','type'=>'select', 'options' => $banksSelect],
-            ['label'=>'Account Type', 'name' => 'account_types_id','id'=>'account-types-id','type'=>'select', 'options' => $accountTypesSelect],
-            ['label'=>'Account Status', 'name' => 'account_status_id','id'=>'account-status-id','type'=>'select', 'options' => $accountStatusSelect],
-            ['label'=>'Account Number', 'name' => 'number','id'=>'number','type'=>'text'],
-            ['label'=>'Current Balance', 'name' => 'current_balance','id'=>'current-balance','type'=>'money'],
-        ],
-        'submit_button_label'=>'Edit Bank Account','url'=>'/api/bankingaccount'
-    ))
-
-{{--Delete banking account form--}}
-@include('layouts.forms.modal_form',
-    array(
-            'id'=>'delete_banking_account_form',
-            'header'=>'Delete Bank Account',
-            'description'=>'',
-            'cancel_button_label'=>'Cancel',
-            'method'=>'delete',
-            'inputs'=>[
-                ['label'=>'','id'=>'delete_banking_account_id','type'=>'hidden']
-            ],
-            'submit_button_label'=>'Delete Bank Account','url'=>'/api/bankingaccount/',
-    ))
-
 {{-- Create car form --}}
 @include('layouts.forms.modal_form',
     array(
@@ -494,21 +493,58 @@
             'inputs'=>[
                 ['label'=>'','id'=>'delete_car_id','type'=>'hidden']
             ],
-            'submit_button_label'=>'Delete car','url'=>'/api/car',))
+            'submit_button_label'=>'Delete car','url'=>'/api/car',
+            ))
 
-{{-- Create cash form --}}
+{{--Create Banking account form--}}
 @include('layouts.forms.modal_form',
     array(
-        'id'=>'modal_cash_form',
-        'header'=>'Create Cash Account',
+        'id'=>'modal_banking_account_form',
+        'header'=>'Create Bank Account',
         'description'=>'',
         'cancel_button_label'=>'Cancel',
         'inputs'=>[
-            ['label'=>'Bank Name','id'=>'cash_bank','type'=>'text'],
-            ['label'=>'Current Balance','id'=>'cash_current_balance','type'=>'money']
+            ['label'=>'Bank Name', 'name' => 'banks_id','id'=>'banks-id','type'=>'select', 'options' => $banksSelect],
+            ['label'=>'Account Type', 'name' => 'account_types_id','id'=>'account-types-id','type'=>'select', 'options' => $accountTypesSelect],
+            ['label'=>'Account Status', 'name' => 'account_status_id','id'=>'account-status-id','type'=>'select', 'options' => $accountStatusSelect],
+            ['label'=>'Account Number', 'name' => 'number','id'=>'number','type'=>'text'],
+            ['label'=>'Current Balance', 'name' => 'current_balance','id'=>'current-balance','type'=>'money'],
         ],
-        'submit_button_label'=>'Create Cash Account','url'=>'/test/forms'
+        'submit_button_label'=>'Create Bank Account','url'=>'/api/bankingaccount'
     ))
+
+{{-- Edit banking account form --}}
+@include('layouts.forms.modal_form',
+    array(
+        'id'=>'modal_edit_banking_account_form',
+        'header'=>'Edit Bank Account',
+        'description'=>'',
+        'method'=>'put',
+        'cancel_button_label'=>'Cancel',
+        'inputs'=>[
+            ['label'=>'Bank Name', 'name' => 'banks_id','id'=>'banks-id','type'=>'select', 'options' => $banksSelect],
+            ['label'=>'Account Type', 'name' => 'account_types_id','id'=>'account-types-id','type'=>'select', 'options' => $accountTypesSelect],
+            ['label'=>'Account Status', 'name' => 'account_status_id','id'=>'account-status-id','type'=>'select', 'options' => $accountStatusSelect],
+            ['label'=>'Account Number', 'name' => 'number','id'=>'number','type'=>'text'],
+            ['label'=>'Current Balance', 'name' => 'current_balance','id'=>'current-balance','type'=>'money'],
+        ],
+        'submit_button_label'=>'Edit Bank Account','url'=>'/api/bankingaccount'
+    ))
+
+{{-- Delete banking account form --}}
+@include('layouts.forms.modal_form',
+    array(
+            'id'=>'delete_banking_account_form',
+            'header'=>'Delete Bank Account',
+            'description'=>'',
+            'cancel_button_label'=>'Cancel',
+            'method'=>'delete',
+            'inputs'=>[
+                ['label'=>'','id'=>'delete_banking_account_id','type'=>'hidden']
+            ],
+            'submit_button_label'=>'Delete Bank Account','url'=>'/api/bankingaccount/',
+    ))
+
 
 {{-- Delete loan form--}}
 @include('layouts.forms.modal_form',
@@ -577,8 +613,13 @@
         return true;
     }
 
-    function createBankigAccount() {
+    function createBankingAccount() {
         $('#modal_banking_account_form').modal('toggle');
+        return true;
+    }
+
+    function createInvestment() {
+        $('#modal_investment_form').modal('toggle');
         return true;
     }
 
@@ -598,7 +639,14 @@
 
     function deleteBankingAccount(banking_account_id_encode){
         $('#delete_banking_account_id').attr('value',banking_account_id_encode);
-        $('#description_delete_car_form').html('Are you sure than you want to delete this banking account?');
+        $('#description_delete_banking_account_form').html('Are you sure than you want to delete this banking account?');
+        $('#delete_banking_account_form').modal('toggle');
+        return true;
+    }
+
+    function deleteInvestment(investment_id_encode){
+        $('#delete_banking_account_id').attr('value',investment_id_encode);
+        $('#description_delete_car_form').html('Are you sure than you want to delete this investment?');
         $('#delete_car_form').modal('toggle');
         return true;
     }
