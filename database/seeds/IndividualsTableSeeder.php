8e4a21d959cae2529a337dd106e1d6ec139788e2
alberto <spin.jag@gmail.com>
2016-10-04 09:09:12 -0500

    fixes name, last name on seed

--- database/seeds/IndividualsTableSeeder.php
+++ database/seeds/IndividualsTableSeeder.php
@@ -15,10 +15,10 @@ class IndividualsTableSeeder extends Seeder
         $dateNow = Carbon::now();
 
         DB::table('individuals')->insert([
-            ['users_id'=>1, 'marital_status_id' => 1, 'name' => 'andres@zoefin.com', 'lastname' => bcrypt('12345678'), 'date_birth' => '1983-01-01', 'gender' => 'M', 'principal' => 1, 'created_at' => $dateNow, 'updated_at' => $dateNow],
-            ['users_id'=>2, 'marital_status_id' => 1, 'name' => 'miguel@zoefin.com', 'lastname' => bcrypt('12345678'), 'date_birth' => '1983-01-01', 'gender' => 'M', 'principal' => 1, 'created_at' => $dateNow, 'updated_at' => $dateNow],
-            ['users_id'=>3, 'marital_status_id' => 1, 'name' => 'alberto@zoefin.com', 'lastname' => bcrypt('12345678'), 'date_birth' => '1983-01-01', 'gender' => 'M', 'principal' => 1, 'created_at' => $dateNow, 'updated_at' => $dateNow],
-            ['users_id'=>4, 'marital_status_id' => 1, 'name' => 'melissa@zoefin.com', 'lastname' => bcrypt('12345678'), 'date_birth' => '1983-01-01', 'gender' => 'F', 'principal' => 1, 'created_at' => $dateNow, 'updated_at' => $dateNow],
+            ['users_id'=>1, 'marital_status_id' => 1, 'name' => 'andres', 'lastname' => 'Garcia', 'date_birth' => '1983-01-01', 'gender' => 'M', 'principal' => 1, 'created_at' => $dateNow, 'updated_at' => $dateNow],
+            ['users_id'=>2, 'marital_status_id' => 1, 'name' => 'miguel', 'lastname' => 'Fruto', 'date_birth' => '1983-01-01', 'gender' => 'M', 'principal' => 1, 'created_at' => $dateNow, 'updated_at' => $dateNow],
+            ['users_id'=>3, 'marital_status_id' => 1, 'name' => 'alberto', 'lastname' => 'Gonzalez', 'date_birth' => '1983-01-01', 'gender' => 'M', 'principal' => 1, 'created_at' => $dateNow, 'updated_at' => $dateNow],
+            ['users_id'=>4, 'marital_status_id' => 1, 'name' => 'melissa', 'lastname' => 'Smith', 'date_birth' => '1983-01-01', 'gender' => 'F', 'principal' => 1, 'created_at' => $dateNow, 'updated_at' => $dateNow],
         ]);
     }
 }
