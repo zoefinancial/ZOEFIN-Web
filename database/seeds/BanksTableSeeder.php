73965997dd64582e0487c012f97c9f272da88e7b
alberto <spin.jag@gmail.com>
2016-10-06 08:53:20 -0500

    Include 12 major banks options

--- database/seeds/BanksTableSeeder.php
+++ database/seeds/BanksTableSeeder.php
@@ -19,6 +19,14 @@ class BanksTableSeeder extends Seeder
             ['name' => 'Bank of America', 'created_at' => $dateNow, 'updated_at' => $dateNow],
             ['name' => 'Wells Fargo', 'created_at' => $dateNow, 'updated_at' => $dateNow],
             ['name' => 'Citigroup', 'created_at' => $dateNow, 'updated_at' => $dateNow],
+            ['name' => 'US Bankcorp', 'created_at' => $dateNow, 'updated_at' => $dateNow],
+            ['name' => 'Bank of New York Mellon', 'created_at' => $dateNow, 'updated_at' => $dateNow],
+            ['name' => 'PNC Bank', 'created_at' => $dateNow, 'updated_at' => $dateNow],
+            ['name' => 'Capital One', 'created_at' => $dateNow, 'updated_at' => $dateNow],
+            ['name' => 'TD Bank', 'created_at' => $dateNow, 'updated_at' => $dateNow],
+            ['name' => 'State Street', 'created_at' => $dateNow, 'updated_at' => $dateNow],
+            ['name' => 'BB&T Bank', 'created_at' => $dateNow, 'updated_at' => $dateNow],
+            ['name' => 'Charles Schwab', 'created_at' => $dateNow, 'updated_at' => $dateNow],
         ]);
     }
 }
