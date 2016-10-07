c8e7cad4d6ea39d2c51495ecb3f144b9fb546b04
Jose Alberto Gonzalez Rouille <spin.jag@gmail.com>
2016-10-06 22:08:33 -0500

    migration incomes

---
+++ database/migrations/2016_10_07_030142_create_incomes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateIncomesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('incomes', function(Blueprint $table)
+        {
+            $table->increments('id');
+            $table->integer('users_id')->index('fk_accounts_users_idx');
+            $table->integer('individuals_id')->index('fk_individuals_individuals_idx');
+            $table->integer('income_types_id')->index('fk_income_types_income_types_idx');
+            $table->integer('fiscal_tax_periods_id')->index('fk_fiscal_tax_periods_fiscal_tax_periods_idx');
+            $table->date('date');
+            $table->decimal('value',32,2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('incomes');
+    }
+}
