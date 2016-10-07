cb40ddad75f936c5c35d98b70dbfd12a77337063
alberto <spin.jag@gmail.com>
2016-10-07 12:06:25 -0500

    Rename class

--- database/migrations/2016_10_07_151121_create_expenses_table.php
+++ database/migrations/2016_10_07_151121_create_expenses_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateInterestExpensesTable extends Migration
+class CreateExpensesTable extends Migration
 {
     /**
      * Run the migrations.
