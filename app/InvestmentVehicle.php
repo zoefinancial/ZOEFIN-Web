6ba7d99352a73b92e7827025fc24758de5a32311
alberto <spin.jag@gmail.com>
2016-10-10 15:47:29 -0500

    Fillable on investment vehicle

--- app/InvestmentVehicle.php
+++ app/InvestmentVehicle.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class InvestmentVehicle extends Model
 {
+    protected $fillable = ['description', 'tax_deferred'];
+
     public function investments()
     {
         return $this->hasMany(Investment::class);
