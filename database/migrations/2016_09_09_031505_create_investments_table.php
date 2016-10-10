6d3bc1c74f4641d68e403e112d35f85d7e421b43
alberto <spin.jag@gmail.com>
2016-10-10 15:48:43 -0500

    Edit migrations investment investmentVehicles

--- database/migrations/2016_09_09_031505_create_investments_table.php
+++ database/migrations/2016_09_09_031505_create_investments_table.php
@@ -16,14 +16,19 @@ class CreateInvestmentsTable extends Migration {
 		{
 			$table->increments('id');
 			$table->integer('users_id')->index('fk_investments_users_idx');
-			$table->integer('individuals_id')->index('fk_investments_individuals_idx');
+			$table->integer('individuals_id')->index('fk_investments_individuals_idx')->nullable();
             $table->integer('investments_id')->index('fk_investments_investments_idx')->nullable();
 			$table->integer('investment_vehicles_id')->index('fk_investments_investment_vehicles_idx');
             $table->integer('investment_companies_id')->index('fk_investments_investment_companies_idx');
-			$table->string('employer', 255)->nullable();
-			$table->integer('total_balance')->nullable();
-            $table->date('initial');
+            $table->integer('account_quovo_id')->nullable();
+            $table->integer('quovo_id')->nullable();
+            $table->boolean('active')->nullable;
+            $table->string('employer', 255)->nullable();
+            $table->string('name', 20)->nullable();
+            $table->decimal('total_balance',16,4)->nullable();
+            $table->date('initial')->nullable();
             $table->date('end')->nullable();
+            $table->timestamp('quovo_last_change')->nullable();
             $table->timestamps();
 		});
 	}
