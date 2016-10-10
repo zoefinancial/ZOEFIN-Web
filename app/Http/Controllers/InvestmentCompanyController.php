7c8ba97e2cdf244ac455248e9caf7eb5899d3601
alberto <spin.jag@gmail.com>
2016-10-10 15:45:12 -0500

    Store investment company

--- app/Http/Controllers/InvestmentCompanyController.php
+++ app/Http/Controllers/InvestmentCompanyController.php
@@ -2,11 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\InvestmentCompany;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
 
 class InvestmentCompanyController extends Controller
 {
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+                'quovo_id' =>     'required',
+                'name' =>     'required',
+            ]);
+        $investmentCompany = new InvestmentCompany($request->all());
+        $investmentCompany->save();
+    }
+
     
 }
