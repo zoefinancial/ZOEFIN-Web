cde1d7441611c24b9b26d4448b423d6c6785ef32
alberto <spin.jag@gmail.com>
2016-10-13 16:23:25 -0500

    Fix save quovo fields

--- app/Loan.php
+++ app/Loan.php
@@ -6,10 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Loan extends Model
 {
-    protected $fillable = ['users_id','amount','comments','details','loan_types_id','number','bank_id'];
+    protected $fillable = ['users_id', 'loan_types_id', 'bank_id', 'account_quovo_id', 'quovo_id', 'active', 'amount', 'interest_rate', 'name', 'comments', 'details', 'quovo_last_change'];
 
     public function getLoanType()
     {
-        return $this->hasOne('App\LoanType','id','loan_types_id');
+        return $this->hasOne('App\LoanType', 'id', 'loan_types_id');
     }
 }
